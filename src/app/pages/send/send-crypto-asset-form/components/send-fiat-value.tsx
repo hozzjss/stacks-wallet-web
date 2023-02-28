950e6a34d55be7a392154b7f0fd65aeb74a30d27
alter-eggo <anastas3993@gmail.com>
2023-02-28 19:22:42 +0400

    feat: add isnumber condition

--- src/app/pages/send/send-crypto-asset-form/components/send-fiat-value.tsx
+++ src/app/pages/send/send-crypto-asset-form/components/send-fiat-value.tsx
@@ -5,25 +5,27 @@ import { useField } from 'formik';
 
 import { MarketData } from '@shared/models/market.model';
 import { Money, createMoneyFromDecimal } from '@shared/models/money.model';
+import { isNumber } from '@shared/utils';
 
 import { baseCurrencyAmountInQuote } from '@app/common/money/calculate-money';
 import { i18nFormatCurrency } from '@app/common/money/format-money';
 
-interface SenfFiatInputProps {
+interface SendFiatInputProps {
   marketData: MarketData;
   assetSymbol?: string;
 }
 
-export function SendFiatValue({ marketData, assetSymbol = '' }: SenfFiatInputProps) {
+export function SendFiatValue({ marketData, assetSymbol = '' }: SendFiatInputProps) {
   const [field] = useField('amount');
   const [assetValue, setAssetValue] = useState<Money>(createMoneyFromDecimal(0, assetSymbol));
 
   useEffect(() => {
     let amount = Number(field.value);
 
-    if (isNaN(amount)) {
+    if (isNaN(amount) || !isNumber(amount) || amount < 0) {
       amount = 0;
     }
+
     const assetAmount = createMoneyFromDecimal(amount, assetSymbol);
     setAssetValue(assetAmount);
   }, [field.value, assetSymbol]);
